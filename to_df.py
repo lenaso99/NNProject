6913a1012ab88333780ce924e86595bd36dcba69
lenaso99 <64419436+lenaso99@users.noreply.github.com>
2021-03-16 12:24:04 +0100

    Update to_df.py

--- to_df.py
+++ to_df.py
@@ -13,36 +13,56 @@ def get_df(tsv, length):
     testing = False
     validating = False
 
+    indices = list()
+    words = list()
+    pos_tags = list()
+
     (rows, columns) = df.shape
     for row in range(rows):
         position = df.iloc[row]["position"]
         word = df.iloc[row]["word"]
         pos = df.iloc[row]["POS"]
+        if position != "*":
+            indices.append(position)
+            words.append(word)
+            pos_tags.append(pos)
+
 
         if training:
-            train = train.append({"position": position, "word": word, "POS": pos}, ignore_index=True)
+            if position == "*":
+                train = train.append({"position": indices, "word": words, "POS": pos_tags}, ignore_index=True)
+                indices = list()
+                words = list()
+                pos_tags = list()
             if row/length > 0.7 and position == "*":
                 training = False
                 testing = True
+                
 
         elif testing:
-            test = test.append({"position": position, "word": word, "POS": pos}, ignore_index=True)
+            if position == "*":
+                test = test.append({"position": indices, "word": words, "POS": pos_tags}, ignore_index=True)
+                indices = list()
+                words = list()
+                pos_tags = list()
             if row/length > 0.85 and position == "*":
                 testing = False
                 validating = True
 
         elif validating:
-            validate = validate.append({"position": position, "word": word, "POS": pos}, ignore_index=True)
+            if position == "*":
+                validate = validate.append({"position": indices, "word": words, "POS": pos_tags}, ignore_index=True)
+                indices = list()
+                words = list()
+                pos_tags = list()
 
-
-    # df.to_csv("seq.tsv", sep="\t", index=False)
     train.to_csv("train.tsv", sep="\t", index=False)
     test.to_csv("test.tsv", sep="\t", index=False)
     validate.to_csv("validate.tsv", sep="\t", index=False)
     return(df)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Input the path to a .tsv file and an output directory for the output files.")
+    parser = argparse.ArgumentParser(description="Input the path to a .tsv file and an output directory for the .info file.")
     parser.add_argument("--tsv", dest="tsv", type=str, default="sequences.tsv")
     parser.add_argument("--output_dir", dest="output_dir", type=str, default=".")
 
