2d7e0a4bb701abb95e339ac13786c6b785e96ea9
LeonieSL <64419479+LeonieSL@users.noreply.github.com>
2021-03-21 18:27:50 +0100

    Update classifiers.py

--- classifiers.py
+++ classifiers.py
@@ -1,60 +1,101 @@
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from transformers import BertTokenizerFast, BertModel
+from transformers import BertModel
+import torch
+import math
 
 
-
-class CNN(nn.Module):
+class TextClassifier(nn.ModuleList):
     def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+      super(TextClassifier, self).__init__()
+      self.embed = BertModel.from_pretrained('bert-base-cased')
+
+      self.dropout = nn.Dropout(0.25)
+      self.kernel_1 = 2
+      self.kernel_2 = 3
+      self.kernel_3 = 4
+      self.kernel_4 = 5
+
+      self.embedding = self.embed(padding_idx=0)
+
+      self.conv_1 = nn.Conv1d(self.seq_len, self.out_size, self.kernel_1, self.stride)
+      self.conv_2 = nn.Conv1d(self.seq_len, self.out_size, self.kernel_2, self.stride)
+      self.conv_3 = nn.Conv1d(self.seq_len, self.out_size, self.kernel_3, self.stride)
+      self.conv_4 = nn.Conv1d(self.seq_len, self.out_size, self.kernel_4, self.stride)
+
+      self.pool_1 = nn.MaxPool1d(self.kernel_1, self.stride)
+      self.pool_2 = nn.MaxPool1d(self.kernel_2, self.stride)
+      self.pool_3 = nn.MaxPool1d(self.kernel_3, self.stride)
+      self.pool_4 = nn.MaxPool1d(self.kernel_4, self.stride)
+
+      self.fc = nn.Linear(self.in_features_fc(), 1)
+
+    def in_features_fc(self):
+
+          out_conv_1 = ((self.embedding_size - 1 * (self.kernel_1 - 1) - 1) / self.stride) + 1
+          out_conv_1 = math.floor(out_conv_1)
+          out_pool_1 = ((out_conv_1 - 1 * (self.kernel_1 - 1) - 1) / self.stride) + 1
+          out_pool_1 = math.floor(out_pool_1)
+
+          out_conv_2 = ((self.embedding_size - 1 * (self.kernel_2 - 1) - 1) / self.stride) + 1
+          out_conv_2 = math.floor(out_conv_2)
+          out_pool_2 = ((out_conv_2 - 1 * (self.kernel_2 - 1) - 1) / self.stride) + 1
+          out_pool_2 = math.floor(out_pool_2)
+
+          out_conv_3 = ((self.embedding_size - 1 * (self.kernel_3 - 1) - 1) / self.stride) + 1
+          out_conv_3 = math.floor(out_conv_3)
+          out_pool_3 = ((out_conv_3 - 1 * (self.kernel_3 - 1) - 1) / self.stride) + 1
+          out_pool_3 = math.floor(out_pool_3)
+
+          out_conv_4 = ((self.embedding_size - 1 * (self.kernel_4 - 1) - 1) / self.stride) + 1
+          out_conv_4 = math.floor(out_conv_4)
+          out_pool_4 = ((out_conv_4 - 1 * (self.kernel_4 - 1) - 1) / self.stride) + 1
+          out_pool_4 = math.floor(out_pool_4)
+
+          return (out_pool_1 + out_pool_2 + out_pool_3 + out_pool_4) * self.out_size
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+      x = self.embedding(x)
 
-def get_bert_embed_matrix(train, test):
-    tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
-    encoded_data_train = tokenizer(train,
-    padding=True,
-    return_tensors='pt',)
-    encoded_data_test = tokenizer(test,
-    padding=True,
-    return_tensors='pt',)
+      x1 = self.conv_1(x)
+      x1 = torch.relu(x1)
+      x1 = self.pool_1(x1)
 
-    input_ids_train = encoded_data_train["input_ids"]
-    input_ids_test = encoded_data_test["input_ids"]
-    bert = BertModel.from_pretrained('bert-base-cased')
-    bert_embeddings = list(bert.children())[0]
-    bert_word_embeddings = list(bert_embeddings.children())[0]
-    mat = bert_word_embeddings.weight.data.numpy()
-    return mat
+      x2 = self.conv_2(x)
+      x2 = torch.relu((x2))
+      x2 = self.pool_2(x2)
+
+      x3 = self.conv_3(x)
+      x3 = torch.relu(x3)
+      x3 = self.pool_3(x3)
+
+      x4 = self.conv_4(x)
+      x4 = torch.relu(x4)
+      x4 = self.pool_4(x4)
+
+      union = torch.cat((x1, x2, x3, x4), 2)
+      union = union.reshape(union.size(0), -1)
+
+      out = self.fc(union)
+      out = self.dropout(out)
+      out = torch.sigmoid(out)
+
+      return out.squeeze()
 
 
 class LSTM(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size, embedder):
+    def __init__(self, hidden_dim, vocab_size, tagset_size, embedder):
         super(LSTM, self).__init__()
         self.hidden_dim = hidden_dim
 
-        self.word_embeddings = embedder(embedding_dim)
+        self.word_embeddings = embedder
 
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim)
 
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
     def forward(self, words):
-        embeds = self.word_embeddings(words)
+        embeds = self.word_embeddings(**words)
         lstm_out, _ = self.lstm(embeds.view(len(words), 1, -1))
         tag_space = self.hidden2tag(lstm_out.view(len(words), -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
